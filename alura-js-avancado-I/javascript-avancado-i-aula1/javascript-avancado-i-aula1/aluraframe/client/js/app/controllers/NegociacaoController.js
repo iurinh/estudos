cee02586cbcc6146adbf86fde04793df9c9c2183
Iuri <iuri.hidalgo@gft.com>
2017-03-17 16:40:00 -0300

    javascript-es6-orientacao-a-objetos-parte-1/task/16527

--- alura-js-avancado-I/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-js-avancado-I/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,17 +4,26 @@ class NegociacaoController {
 		let $ = document.querySelector.bind(document);//Mantem o contexto do document para o seletor	
 
 		//Evita percorrer o DOM todas as vezes que for chamar uma funcao
-		this.inputData = $('#data');
-		this.inputQuantidade = $('#quantidade');
-		this.inputValor = $('#valor');
+		this._inputData = $('#data');
+		this._inputQuantidade = $('#quantidade');
+		this._inputValor = $('#valor');
 	}
 
 	adiciona(event){
 		event.preventDefault();
 
-		console.log(this.inputData.value);
-		console.log(this.inputQuantidade.value);
-		console.log(this.inputValor.value);
+		// let data = new Date(this._inputData.value.split('-'));//ano-mes-dia
+		let data = new Date(this._inputData.value.replace(/-/g,','));//ano-mes-dia
+		console.log(data);
+		// let negociacao = new Negociacao(
+		// 	this._inputData.value,
+		// 	this._inputQuantidade.value,
+		// 	this._inputValor.value
+		// );
+
+		// console.log(negociacao);
 	}
+
+
 
 }
