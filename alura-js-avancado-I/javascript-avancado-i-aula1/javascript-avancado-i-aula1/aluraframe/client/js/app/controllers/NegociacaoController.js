4cf941b1c9e89961d62517c80df38f72f838f10a
Iuri N H <Iuri N H>
2017-03-18 14:56:32 -0300

    javascript-es6-orientacao-a-objetos-parte-1/task/16549

--- alura-js-avancado-I/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-js-avancado-I/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,20 +7,34 @@ class NegociacaoController {
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
+		this._listaNegociacoes = new ListaNegociacoes();
 	}
 
 	adiciona(event){
 		event.preventDefault();
-		
-		let negociacao = new Negociacao(
+			
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._limpaFormulario();
+
+
+		console.log(this._listaNegociacoes.negociacoes);
+	}
+
+	//Possui underline pq indica q somente quem faz parte desse controller eh q pode chamar essa funcao
+	_criaNegociacao(){
+		return new Negociacao(
 			DateHelper.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
 			this._inputValor.value
 		);
-		
-		let diamesAno = DateHelper.dataParaTexto(negociacao.data);
-
-		console.log(diamesAno);
 	}
 
+	//Possui underline pq indica q somente quem faz parte desse controller eh q pode chamar essa funcao
+	_limpaFormulario(){
+		this._inputData.value = ""
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0;
+
+		this._inputData.focus();
+	}
 }
