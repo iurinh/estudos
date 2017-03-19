f1ad9a4bc70eda0af285594bef5ecdc793b2a5b5
Iuri N H <Iuri N H>
2017-03-19 18:06:56 -0300

    javascript-es6-orientacao-a-objetos-parte-1/task/16579

--- alura-js-avancado-I/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ alura-js-avancado-I/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,9 +1,9 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
 	constructor(elemento){
-		this._elemento = elemento;
+		super(elemento);
 	}
-
+	
 	_template(model){
 		return `
 				<table class="table table-hover table-bordered">
@@ -37,7 +37,4 @@ class NegociacoesView {
 				`;
 	}
 
-	update(model){
-		this._elemento.innerHTML = this._template(model);
-	}
 }
