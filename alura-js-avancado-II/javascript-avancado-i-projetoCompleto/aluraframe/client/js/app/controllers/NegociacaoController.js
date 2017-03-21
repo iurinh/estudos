f0e10d4330fc27eabba6366a6d4039267a140988
Iuri <iuri.hidalgo@gft.com>
2017-03-21 14:40:21 -0300

    C:/Program Files/Git/javascript-es6-orientacao-a-objetos-parte-2/task/17493

--- alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,25 +7,20 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacoes(),
-            ['adiciona','esvazia'],
-            model => {this._negociacoesView.update(model)}
-        );
-
-        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            this._negociacoesView,
+            'adiciona','esvazia'
+        );
         
-        this._mensagem = new Mensagem();
-        this._mensagem = ProxyFactory.create(
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagem = new Bind(
             new Mensagem(),
-            ['texto'],
-            model => {this._mensagemView.update(model);}
+            this._mensagemView,
+            'texto'
         );
 
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
         
     }
     
