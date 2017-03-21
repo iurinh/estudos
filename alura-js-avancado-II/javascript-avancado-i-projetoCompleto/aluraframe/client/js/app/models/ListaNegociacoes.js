695b244a46960fa629716fb28077f0c133ae4910
Iuri <iuri.hidalgo@gft.com>
2017-03-21 10:25:46 -0300

    javascript-es6-orientacao-a-objetos-parte-2/task/17450

--- alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,28 @@
 class ListaNegociacoes {
     
-    constructor() {
+    constructor(contexto, armadilha) {
         
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        this._contexto = contexto;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
+      	//this._armadilha(this);
+      	//Reflete o contexto e executa a funcao informando quem será o 'this' da operação
+      	Reflect.apply(this._armadilha, this._contexto,[this]);
     }
     
     get negociacoes() {
         
         return [].concat(this._negociacoes);
+    }
+
+    esvazia(){//Apaga todas as negociacoes
+    	this._negociacoes = [];
+      	//this._armadilha(this);
+      	Reflect.apply(this._armadilha, this._contexto,[this]);
     }
 }
