674ec70b7494512f386a66eab07f5dba2ed0413b
Iuri <iuri.hidalgo@gft.com>
2017-03-21 11:31:25 -0300

    javascript-es6-orientacao-a-objetos-parte-2/task/17469 - exemplo de interceptacao com new Proxy()

--- alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,13 @@
 class ListaNegociacoes {
     
-    constructor(armadilha) {
+    constructor() {
         
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-      	this._armadilha(this);
     }
     
     get negociacoes() {
@@ -17,8 +15,7 @@ class ListaNegociacoes {
         return [].concat(this._negociacoes);
     }
 
-    esvazia(){//Apaga todas as negociacoes
+    esvazia(){
     	this._negociacoes = [];
-      	this._armadilha(this);
     }
 }
