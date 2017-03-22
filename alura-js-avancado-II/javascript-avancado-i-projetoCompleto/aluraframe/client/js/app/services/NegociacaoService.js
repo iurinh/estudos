78ef3b1766eed236aa6e2473e052d8a1343ee2b3
Iuri N H <Iuri N H>
2017-03-21 21:43:49 -0300

    javascript-es6-orientacao-a-objetos-parte-2/task/17524

--- alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/services/NegociacaoService.js
+++ alura-js-avancado-II/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/services/NegociacaoService.js
@@ -3,6 +3,18 @@ class NegociacaoService {
 		this._http = new HttpService();
 	}
 
+	obterNegociacoes(){
+		return Promise.all([
+            this.obterNegociacoesDaSemana(),
+            this.obterNegociacoesDaSemanaAnterior(),
+            this.obterNegociacoesDaSemanaRetrasada()
+        ])
+        .then(periodos => {
+            let negociacoes = periodos.reduce((dados, periodo) => dados.concat(periodo),[])
+            return negociacoes;
+        })
+	}
+
 	obterNegociacoesDaSemana() {
 
         return this._http
