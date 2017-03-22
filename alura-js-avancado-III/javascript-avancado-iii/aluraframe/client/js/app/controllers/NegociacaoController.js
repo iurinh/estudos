b09e04d7b8cfcf59cffaf9feb3a10fca69249f4b
Iuri <iuri.hidalgo@gft.com>
2017-03-22 12:32:51 -0300

    javascript-es6-orientacao-a-objetos-parte-3/task/20282

--- alura-js-avancado-III/javascript-avancado-iii/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-js-avancado-III/javascript-avancado-iii/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -20,21 +20,35 @@ class NegociacaoController {
         );
 
         this._ordemAtual = '';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDAO(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao => 
+                    this._listaNegociacoes.adiciona(negociacao)
+                )
+            );
     }
     
     adiciona(event) {
-        
         event.preventDefault();
 
-        try {
-            this._listaNegociacoes.adiciona(this._criaNegociacao());
-            
-            this._mensagem.texto = 'Negociação adicionada com sucesso';
-            
-            this._limpaFormulario(); 
-        } catch(erro) {
-            this._mensagem.texto = erro;
-        }
+        ConnectionFactory
+            .getConnection()
+            .then(connection => {
+                let negociacao = this._criaNegociacao();
+                new NegociacaoDAO(connection)
+                    .adiciona(negociacao)
+                    .then(() => {
+                        this._listaNegociacoes.adiciona(negociacao);
+                        this._mensagem.texto = 'Negociação adicionada com sucesso';
+                        this._limpaFormulario(); 
+                    })
+                    .catch(erro => this._mensagem.texto = erro);
+            });
+
     }
 
     ordena(coluna) {
@@ -68,8 +82,9 @@ class NegociacaoController {
         
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);    
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );    
     }
     
     _limpaFormulario() {
