aad4f121617ea969332217ca2cc7d9c819ffc57d
Iuri <iuri.hidalgo@gft.com>
2017-03-22 17:28:51 -0300

    javascript-es6-orientacao-a-objetos-parte-3/task/20292

--- alura-js-avancado-III/javascript-avancado-iii/aluraframe/client/js/app/services/HttpService.js
+++ alura-js-avancado-III/javascript-avancado-iii/aluraframe/client/js/app/services/HttpService.js
@@ -8,36 +8,19 @@ class HttpService {
     }
 
     get(url) {
-        url ='w';
-        return fetch(url)//Nao tem como cancelar a requisição, se precisar de algo assim, deverá fazer o modo antigo
+        return fetch(url)
             .then(res => this._handleErrors(res) )
             .then(res => res.json() )
     }
 
     post(url, dado) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send(JSON.stringify(dado));
-        });
+        return fetch(url,{
+            headers: {'Content-type' : 'application/json'},
+            method:  'post',
+            body:    JSON.stringify(dado)
+        })
+            .then(res => this._handleErrors(res) )
 
     }
 }
