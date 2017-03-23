158f57937377b7151d9e1800ef2cbfbf2a98c424
Iuri <iuri.hidalgo@gft.com>
2017-03-23 09:53:12 -0300

    javascript-es6-orientacao-a-objetos-parte-3/task/20296

---
+++ alura-js-avancado-III/javascript-avancado-iii/aluraframe/client/js/app/helpers/Bind.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Bind = function Bind(model, view) {
+  _classCallCheck(this, Bind);
+
+  for (var _len = arguments.length, props = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    props[_key - 2] = arguments[_key];
+  }
+
+  //REST Operator, parece com Spread Operator
+  var proxy = ProxyFactory.create(model, props, function (model) {
+    return view.update(model);
+  });
+
+  view.update(model);
+
+  return proxy;
+};
+//# sourceMappingURL=Bind.js.map
