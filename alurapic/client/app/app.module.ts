7431f55c0c05d363bab3e0c8fedd6c289e13ffc1
Iuri <iuri.hidalgo@gft.com>
2017-03-24 11:38:42 -0300

    angular2-parte1/task/21370

--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpModule} from '@angular/http'
+import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
+import {PainelModule} from './painel/painel.module';
 import 'rxjs/add/operator/map';
 
 @NgModule({
-    imports:[BrowserModule, FotoModule, HttpModule],
+    imports:[BrowserModule, FotoModule, HttpModule, PainelModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]//inicializador
 })
