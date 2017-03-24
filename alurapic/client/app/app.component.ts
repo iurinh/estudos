4208b4bdc68dd85e05e045c1bb5e5b0f6e05ff27
Iuri <iuri.hidalgo@gft.com>
2017-03-24 10:17:56 -0300

    angular2-parte1/task/21365

--- alurapic/client/app/app.component.ts
+++ alurapic/client/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 
 @Component({
+    moduleId: module.id,//Com isso o angular procura o arquivo a partir da pasta atual desse script
     selector: 'app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
 
