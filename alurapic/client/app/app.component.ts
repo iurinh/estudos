446052156044940d366e0baaae365ab42ab57761
Iuri <iuri.hidalgo@gft.com>
2017-03-24 10:47:00 -0300

    angular2-parte1/task/21368

--- alurapic/client/app/app.component.ts
+++ alurapic/client/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
     moduleId: module.id,//Com isso o angular procura o arquivo a partir da pasta atual desse script
@@ -7,4 +8,14 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+    // fotos: Array<Object> = []; Tambem pode ser assim!!!
+    fotos: Object[] = [];
+
+    constructor(http: Http){//Angular2 compreende que precisa injetar a dependencia de Http, mas precisa de um provider. Esses ":" indica o tipo da variavel, nao precisa de @Inject
+        let stream = http.get('v1/fotos');
+        stream.subscribe(res => {
+            this.fotos = res.json();
+            console.log(this.fotos);
+        });
+    }
 }
