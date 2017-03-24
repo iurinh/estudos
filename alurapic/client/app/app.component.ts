c2338183969a950771095123d4c1db27b7d5bd8a
Iuri <iuri.hidalgo@gft.com>
2017-03-24 12:20:18 -0300

    angular2-parte1/task/21372

--- alurapic/client/app/app.component.ts
+++ alurapic/client/app/app.component.ts
@@ -1,22 +1,8 @@
 import {Component} from '@angular/core';
-import {Http} from '@angular/http';
 
 @Component({
     moduleId: module.id,//Com isso o angular procura o arquivo a partir da pasta atual desse script
     selector: 'app',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
-
-    // fotos: Array<Object> = []; Tambem pode ser assim!!!
-    fotos: Object[] = [];
-
-    constructor(http: Http){//Angular2 compreende que precisa injetar a dependencia de Http, mas precisa de um provider. Esses ":" indica o tipo da variavel, nao precisa de @Inject
-        http.get('v1/fotos')
-        .map(res => res.json())
-        .subscribe(
-            fotos => this.fotos = fotos, 
-            erro => console.log(erro)
-        );
-    }
-}
+export class AppComponent {}
--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -4,13 +4,20 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
 import {PainelModule} from './painel/painel.module';
+import {CadastroComponent} from './cadastro/cadastro.component';
+import {ListagemComponent} from './listagem/listagem.component';
+import {routing} from './app.routes';
 import 'rxjs/add/operator/map';
 
 @NgModule({
-    imports:[BrowserModule, FotoModule, HttpModule, PainelModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]//inicializador
+    imports:[   
+        BrowserModule, 
+        FotoModule, 
+        HttpModule, 
+        PainelModule,
+        routing
+    ],
+    declarations: [AppComponent, CadastroComponent, ListagemComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule{
-
-}
+export class AppModule{ }
