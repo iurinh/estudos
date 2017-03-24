10ff6d3c43665c76de07fa94e3b96562c4adb2bc
Iuri <iuri.hidalgo@gft.com>
2017-03-24 15:35:59 -0300

    angular2-parte1/task/21378

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FotoComponent} from '../foto/foto.component'
 
 @Component({
     moduleId: module.id,
@@ -7,4 +8,9 @@ import {Component} from '@angular/core';
 })
 export class CadastroComponent{
 
+    foto: FotoComponent = new FotoComponent();
+
+    cadastrar(){
+        console.log(this.foto);
+    }
 }
