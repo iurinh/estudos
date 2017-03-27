b1a2a5429f6f10a0f0e65c3fa759feff4061ec83
Iuri <iuri.hidalgo@gft.com>
2017-03-27 11:03:42 -0300

    angular2-parte2/task/21425

--- alurapic/client/app/foto/foto.module.ts
+++ alurapic/client/app/foto/foto.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {FotoComponent} from './foto.component';
 import {FiltroPorTitulo} from './foto.pipes';
+import {FotoService} from './foto.service'
 
 @NgModule({
-    declarations: [FotoComponent, FiltroPorTitulo],//O q o modulo tem
-    exports: [FotoComponent, FiltroPorTitulo]//O que os outros modulos q dependem desse podem usar 
+    declarations: [FotoComponent, FiltroPorTitulo],
+    exports: [FotoComponent, FiltroPorTitulo],
+    providers: [FotoService]//Nao eh um pipe e nao tem decorator, por isso apenas em 'providers'
 })
 export class FotoModule{
 
