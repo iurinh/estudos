700841be67f9b7f441a2613990bc9bad745f6798
Iuri <iuri.hidalgo@gft.com>
2017-03-27 15:49:05 -0300

    angular2-parte2/task/21444

--- alurapic/client/app/listagem/listagem.component.ts
+++ alurapic/client/app/listagem/listagem.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {FotoService} from '../foto/foto.service';
 import {FotoComponent} from '../foto/foto.component';
+import {PainelComponent} from '../painel/painel.component';
 
 @Component({
     moduleId: module.id,
@@ -25,20 +26,22 @@ export class ListagemComponent {
             );
     }
     
-    remover(foto: FotoComponent){
+    remover(foto: FotoComponent, painel: PainelComponent){
         this.service
             .remove(foto)
             .subscribe(
                 () => {
-                    console.log('Foto removida com sucesso')
-                    let novasFotos = this.fotos.slice(0);//Slice copia num novo array
+                    painel.fadeOut(() => {
+                        console.log('Foto removida com sucesso')
+                        let novasFotos = this.fotos.slice(0);//Slice copia num novo array
 
-                    let indice = novasFotos.indexOf(foto);
-                    novasFotos.splice(indice, 1);
+                        let indice = novasFotos.indexOf(foto);
+                        novasFotos.splice(indice, 1);
 
-                    this.fotos = novasFotos;
+                        this.fotos = novasFotos;
 
-                    this.mensagem = 'Foto removida com sucesso';
+                        this.mensagem = 'Foto removida com sucesso';
+                    });
                 },
                 erro => {
                     console.log(erro);
