10f0ad90b96851fa1f73357ea2229712c72d3106
Iuri <iuri.hidalgo@gft.com>
2017-03-29 14:38:36 -0300

    node-js/task/12868 testes

--- teste-node/casadocodigo/test/produtos.js
+++ teste-node/casadocodigo/test/produtos.js
@@ -1,5 +1,24 @@
+var http = require('http');
+
 describe('#ProdutosController', function(){
-    it('#listagem json', function(){
-        console.log('teste de verificacao de listagem json')
+    it('#listagem json', function(done){
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        http.get(configuracoes,function(res){
+            if(res.statusCode== 200)
+                console.log('STatus OK');
+            
+            if(res.headers['content-type'] == "application/json/charset=utf-8")
+                console.log('Content type ok');
+
+            done();
+        })
     })
 })
