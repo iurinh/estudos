286aec87e65ab082fbd9d024cafa003dd089d578
Iuri <iuri.hidalgo@gft.com>
2017-03-29 18:05:10 -0300

    node-js/task/12892

--- teste-node/casadocodigo/app/routes/home.js
+++ teste-node/casadocodigo/app/routes/home.js
@@ -1,12 +1,10 @@
-module.exports = function(app){
-    app.get('/', function(req, res){
+module.exports = function(app) {
+    app.get('/', function(req, res) {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
-
-        produtosDAO.lista(function(erro,resultados){
+        produtosDAO.lista(function(erros, resultados) {
             res.render('home/index',{livros:resultados});
         });
-
         connection.end();
-    })
+    });
 }
