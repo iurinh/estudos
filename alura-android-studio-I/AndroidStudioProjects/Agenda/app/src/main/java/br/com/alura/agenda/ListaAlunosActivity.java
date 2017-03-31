bbc3a8b9b9fa7072a04567481009ddb24db88349
Iuri <iuri.hidalgo@gft.com>
2017-03-31 15:13:47 -0300

    android-studio/task/11355

--- alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -8,6 +8,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.List;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
 public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +20,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Daniel", "Ronaldo", "Jefferson", "Felipe", "Daniel", "Ronaldo", "Jefferson", "Felipe", "Daniel", "Ronaldo", "Jefferson", "Felipe", "Ronaldo", "Jefferson", "Felipe"};
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos= dao.buscaAlunos();
+        dao.close();
 
         ListView listaAlunos = (ListView) findViewById(R.id.listaAlunos);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
 
         Button novoAluno = (Button) findViewById(R.id.novo_aluno);
--- alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -2,8 +2,12 @@ package br.com.alura.agenda.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.alura.agenda.modelo.Aluno;
 
@@ -22,10 +26,10 @@ public class AlunoDAO extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         String sql = "CREATE TABLE alunos (" +
                     " id INTEGER PRIMARY KEY," +
-                    " nome TEXT NOT NULL" +
-                    " endereco TEXT" +
-                    " telefone TEXT" +
-                    " site TEXT" +
+                    " nome TEXT NOT NULL," +
+                    " endereco TEXT," +
+                    " telefone TEXT," +
+                    " site TEXT," +
                     " nota REAL" +
                     ")";
 
@@ -52,5 +56,30 @@ public class AlunoDAO extends SQLiteOpenHelper{
         dados.put("nota", aluno.getNota());
 
         db.insert("alunos", null, dados);
+    }
+
+    public List<Aluno> buscaAlunos() {
+        String sql = "SELECT * FROM alunos";
+
+        SQLiteDatabase db =getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+
+        List<Aluno> alunos = new ArrayList<Aluno>();
+        Aluno aluno;
+        while(cursor.moveToNext()){
+            aluno = new Aluno();
+
+            aluno.setId(cursor.getLong(cursor.getColumnIndex("id")));
+            aluno.setNome(cursor.getString(cursor.getColumnIndex("nome")));
+            aluno.setEndereco(cursor.getString(cursor.getColumnIndex("endereco")));
+            aluno.setTelefone(cursor.getString(cursor.getColumnIndex("telefone")));
+            aluno.setSite(cursor.getString(cursor.getColumnIndex("site")));
+            aluno.setNota(cursor.getDouble(cursor.getColumnIndex("nota")));
+
+            alunos.add(aluno);
+        }
+        cursor.close();
+
+        return alunos;
     }
 }
