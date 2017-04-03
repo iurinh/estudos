1cc51ddc1e550e4baafa4cd62682236596ff39c6
Iuri <iuri.hidalgo@gft.com>
2017-04-03 16:04:06 -0300

    android-studio-ii-integracoes-e-recursos/task/11800

--- alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
+++ alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
@@ -1,6 +1,10 @@
 package br.com.alura.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -9,6 +13,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import br.com.alura.agenda.R;
 import br.com.alura.agenda.modelo.Aluno;
 
 /**
@@ -41,10 +46,26 @@ public class AlunosAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(context);
-
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.list_item, null);
+
+        TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if(caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = bitmap.createScaledBitmap(bitmap, 100, 100, true);
+
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
 
         return view;
     }
