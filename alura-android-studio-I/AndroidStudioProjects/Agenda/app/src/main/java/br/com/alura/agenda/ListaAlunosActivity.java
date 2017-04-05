42e253197e3adf45b27bad21eb86ab8710773a15
Iuri <iuri.hidalgo@gft.com>
2017-04-05 17:38:36 -0300

    android-studio-iii-fragments-maps-gps/task/11904

--- alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ alura-android-studio-I/AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -89,7 +89,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 break;
 
             case R.id.menu_mapa:
-                Intent vaiParaMapa = new Intent(this, MapsActivity.class);
+                Intent vaiParaMapa = new Intent(this, MapaActivity.class);
                 startActivity(vaiParaMapa);
                 break;
         }
