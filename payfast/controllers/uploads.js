cad8dfae046a31a7ad56659fbd9ccb8c2f70823a
Iuri <iuri.hidalgo@gft.com>
2017-04-10 10:30:09 -0300

    nodejs-avancado/task/20118

---
+++ payfast/controllers/uploads.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+module.exports = function(app){
+  app.post('/upload/imagem', function(req, res){
+    console.log('Recebendo imagem...');
+
+    var fileName = req.headers.filename;
+    req.pipe(fs.createWriteStream('files/' + fileName))
+       .on('finish', function(){
+         console.log('Finalizou escrita');
+         res.status(200).send('ok');
+       });
+  });
+}
