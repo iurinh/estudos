56dcf3dc37233dc8b3cc4c0300e998a45e9e1aeb
Iuri <iuri.hidalgo@gft.com>
2017-04-24 10:03:54 -0300

    Strategy design-patterns/task/234

--- alura-design-pattern-I/design-pattern/src/CalculadorDeImposto.java
+++ alura-design-pattern-I/design-pattern/src/CalculadorDeImposto.java
@@ -1,9 +1,9 @@
 
 public class CalculadorDeImposto {
 
-	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer){
-		double icms = impostoQualquer.calcula(orcamento);
-		System.out.println(icms);
+	public void realizaCalculo(Orcamento orcamento, Imposto imposto){
+		double valor = imposto.calcula(orcamento);
+		System.out.println(valor);
 	}
 
 }
