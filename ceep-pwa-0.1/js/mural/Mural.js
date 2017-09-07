5979e7fbe34a9d90194fb62153013723b0fd1855
Emy <m.iogurt@gmail.com>
2017-09-07 11:30:43 -0300

    progressive-webapp-offline/task/25258

--- ceep-pwa-0.1/js/mural/Mural.js
+++ ceep-pwa-0.1/js/mural/Mural.js
@@ -2,6 +2,9 @@ const Mural = (function(_render, Filtro){
     "use strict"
     let cartoes = JSON.parse(localStorage.getItem("cartoes") || "[]").map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    cartoes.forEach(cartao => {
+        preparaCartao(cartao)
+    })
 
     render();
 
@@ -13,17 +16,22 @@ const Mural = (function(_render, Filtro){
         ))
     }
 
+    function preparaCartao(cartao){
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", ()=>{
+            cartoes = cartoes.slice(0)
+            cartoes.splice(cartoes.indexOf(cartao),1)
+            salvaCartoes()
+            render()
+        })
+        render()
+    }
+
     function adiciona(cartao){
         if(logado){
             cartoes.push(cartao)
             salvaCartoes()
-            cartao.on("mudanca.**", render)
-            cartao.on("remocao", ()=>{
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao),1)
-                render()
-            })
-            render()
+            preparaCartao(cartao)
             return true
         } else {
             alert("Você não está logado")
