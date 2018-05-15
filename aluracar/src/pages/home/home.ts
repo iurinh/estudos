1d400e97efc76bf7b29c5c0990d8888ae3143c5f
Iuri do N H <iurinh@gmail.com>
2018-05-14 21:44:35 -0300

    Caso de erro http, mostrar mensagem

--- aluracar/src/pages/home/home.ts
+++ aluracar/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
 
 @Component({
@@ -14,11 +14,12 @@ export class HomePage {
   constructor(
     public navCtrl: NavController, 
     private _http: HttpClient,
-    private _loadingCtrl: LoadingController
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController
   ) {
     
     let loading = this._loadingCtrl.create({
-      content: 'Aguarde o carregamento'
+      content: 'Carregando carros...'
     })
 
     loading.present()
@@ -27,8 +28,16 @@ export class HomePage {
     .subscribe((carros) => { 
       this.carros = carros;
       loading.dismiss();
-    });
-    
+    }, (erro) => {
+      loading.dismiss();
+      this._alertCtrl.create({
+        title: 'Falha de conexão',
+        subTitle: 'Não foi possivel carregar os carros',
+        buttons: [
+          { text: 'Ok'}
+        ]
+      }).present();
+    })
   }
 
 }
