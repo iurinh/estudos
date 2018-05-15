3d75d09265fa2995609c0a263b6481006a73bb20
Iuri do N H <iurinh@gmail.com>
2018-05-14 21:39:13 -0300

    Carregamendo com loading

--- aluracar/src/pages/home/home.ts
+++ aluracar/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
 
 @Component({
@@ -11,10 +11,23 @@ export class HomePage {
 
   public carros: Carro[];
 
-  constructor(public navCtrl: NavController, private _http: HttpClient) {
+  constructor(
+    public navCtrl: NavController, 
+    private _http: HttpClient,
+    private _loadingCtrl: LoadingController
+  ) {
     
+    let loading = this._loadingCtrl.create({
+      content: 'Aguarde o carregamento'
+    })
+
+    loading.present()
+
     this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
-    .subscribe((carros) => { this.carros = carros; });
+    .subscribe((carros) => { 
+      this.carros = carros;
+      loading.dismiss();
+    });
     
   }
 
