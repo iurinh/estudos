d933f84b94f007f965d300159727d37f391c39f7
Iuri NH <iurinh@gmail.com>
2018-07-06 00:17:06 -0300

    While, try/catch

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,16 +2,23 @@ print("***********************")
 print("Bem vindo ! Adivinhação é o Jogo!")
 print("***********************")
 
+total_tentativas = 3;
+tentativas = 1
+game_over = total_tentativas == tentativas+1
+
 numero_secreto = 42
 
 chute = 0
 
 acertou = chute == numero_secreto
 
-while not acertou:
+while not acertou and not game_over:
     try:
-        chute = input("Digite o numero: ")
+        chute = input("Digite o numero ({0}/{1}): ".format(tentativas, total_tentativas))
         chute = float(chute);
+
+        tentativas = tentativas + 1
+        game_over = (total_tentativas+1 == tentativas)
 
         acertou = chute == numero_secreto
         maior = chute > numero_secreto
@@ -25,3 +32,6 @@ while not acertou:
 
     except ValueError:
         print("Digite somente numeros")
+else:
+    if not acertou and game_over:
+        print("Você não conseguiu - GAME OVER")
