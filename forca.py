dd436037ace16dc012afc01e63fb9351e1fb7fa8
Iuri NH <iurinh@gmail.com>
2018-07-09 09:34:52 -0300

    Pegando texto e verificando na palabra secreta

--- forca.py
+++ forca.py
@@ -5,5 +5,21 @@ def jogar():
     print("Bem vindo ! Fogo da Forca!")
     print("***********************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual letra?").strip()
+
+        idx = 0
+        for  letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print(letra, idx)
+
+            idx = idx + 1
+
+        print("Jogando")
+
 if(__name__ == "__main__"):
     jogar()
