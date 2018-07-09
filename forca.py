bc1e96e5be3b1b6fa8c92ef7c7d9e91f22492430
Iuri NH <iurinh@gmail.com>
2018-07-09 15:36:53 -0300

    Adicao de contagem de erros e finalizacao de jogo

--- forca.py
+++ forca.py
@@ -5,21 +5,35 @@ def jogar():
     print("Bem vindo ! Fogo da Forca!")
     print("***********************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadads = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     while not enforcou and not acertou:
-        chute = input("Qual letra?").strip()
+        chute = input("Qual letra?").strip().upper()
 
-        idx = 0
-        for  letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print(letra, idx)
+        if chute in palavra_secreta:
+            idx = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadads[idx] = chute
 
-            idx = idx + 1
+                idx += 1
 
-        print("Jogando")
+            print("Quantidade de letras encontradas: ", palavra_secreta.count(chute))
+            print(letras_acertadads)
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadads
+
+    if acertou:
+        print("Parabéns!")
+    else:
+        print("GAME OVER")
 
 if(__name__ == "__main__"):
     jogar()
