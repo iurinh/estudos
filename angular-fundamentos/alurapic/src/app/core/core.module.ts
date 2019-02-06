ef87f0f56b1af3dfb9955d748509595dec1a5b49
Iuri NH <iurinh@gmail.com>
2019-02-06 20:20:54 -0200

    Adicao de projeto final

--- angular-fundamentos/alurapic/src/app/core/core.module.ts
+++ angular-fundamentos/alurapic/src/app/core/core.module.ts
@@ -4,13 +4,29 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './auth/request.interceptor';
+import { FooterComponent } from './footer/footer.component';
+import { AlertModule } from '../shared/components/alert/alert.module';
+import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedDirective } from '../shared/directives/show-if-logged/show-if-logged.directive';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
-    declarations: [HeaderComponent],
-    exports: [HeaderComponent],
+    declarations: [
+        HeaderComponent,
+        FooterComponent
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent
+    ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        AlertModule,
+        LoadingModule,
+        MenuModule,
+        ShowIfLoggedModule
     ],
     providers: [
         {
--- angular-fundamentos/alurapic/src/app/home/home.routing.module.ts
+++ angular-fundamentos/alurapic/src/app/home/home.routing.module.ts
@@ -5,20 +5,27 @@ import { HomeComponent } from './home.component';
 import { AuthGuard } from '../core/auth/auth.guard';
 import { SignInComponent } from './signin/signin.component';
 import { SignUpComponent } from './singup/singup.component';
+import { LoginGuard } from '../core/auth/login.guard';
 
 const routes: Routes = [
     { 
         path: '',
         component: HomeComponent,
-        canActivate: [AuthGuard],
+        canActivate: [LoginGuard],
         children: [
             { 
                 path: '',
                 component: SignInComponent,
+                data: {
+                    title: 'Sign in'
+                }
             }, 
             { 
                 path: 'signup',
                 component: SignUpComponent,
+                data: {
+                    title: 'Sign up'
+                }
             },            
         ]
     },              
