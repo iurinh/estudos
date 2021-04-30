2aa36310701fec59e2a6d1ddde12e1e14c62cdeb
Iuri <iurinh@gmail.com>
2021-04-29 21:24:23 -0300

    Capitulo 1: + Dispatchers e SQLite

--- intelliJ-workspace/ecommerce/service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ intelliJ-workspace/ecommerce/service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,9 +2,13 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService {
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
 
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
@@ -18,7 +22,7 @@ public class FraudDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processando");
         System.out.println(record.key());
@@ -32,6 +36,20 @@ public class FraudDetectorService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        Order order = record.value();
+
+        if(isFraud(order)) {
+            System.out.println("É uma FRAUDE" + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+        } else {
+            System.out.println("Tranquilo" + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+        }
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 
 }
---
+++ intelliJ-workspace/ecommerce/service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
@@ -0,0 +1,65 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+public class CreateUserService {
+
+    private final Connection connection;
+
+    CreateUserService() throws SQLException {
+        String url = "jdbc:sqlite:target/users_database.db";
+        this.connection = DriverManager.getConnection(url);
+        connection.createStatement().execute(
+                "create table Users (" +
+                "uuid varchar(200) primary key, " +
+                "email varchar(200))");
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var createUserService = new CreateUserService();
+        try(var service = new KafkaService<>(
+                CreateUserService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                createUserService::parse,
+                Order.class,
+                new HashMap<String, String>())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException, SQLException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processando novo usuario");
+        System.out.println(record.value());
+
+       Order order = record.value();
+
+        if(isNewUser(order.getEmail()))
+            insertNewUser(order.getEmail());
+    }
+
+    private void insertNewUser(String email) throws SQLException {
+        var insert = connection.prepareStatement("insert into Users (uuid, email) values (?,?)");
+        insert.setString(1, "uuid");
+        insert.setString(2, "email");
+        insert.execute();
+        System.out.println("Usuario Adicionado " + email);
+    }
+
+    private boolean isNewUser(String email) throws SQLException {
+        var exists = connection.prepareStatement("select uuid from Users where email == ? limit 1");
+        exists.setString(1, email);
+
+        var result = exists.executeQuery();
+
+        return !result.next();
+    }
+
+}
